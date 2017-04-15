4df842a9b8bb9295554fd2c0f36bc06cd327cc18
teeyingyap <yapteeying@gmail.com>
2017-04-15 23:04:19 +0800

    first commit

--- public/js/application.js
+++ public/js/application.js
@@ -29,13 +29,46 @@ $(document).ready(function() {
 
         error: function(data) {    
         	$('#spinner').hide();
-            $('form input').val('Invalid Url!');    
-        	$('#flash').html(data.responseText)     
-        }    
+            $('form input').val('Invalid URL!');    
+        	$('#flash').html(data.responseText)
+              .show()
+              .fadeOut(2000);
+        },    
 
-        
+        complete: function() {      
+            $('#button').val('SHORTEN');     
+        }
+
      
     }); // end of function .ajax
   }); // end of function .submit
-}); // end of function document.ready
+
+$(".link").click(function(e) {
+    var target = $(e.target).parent().siblings().last()
+    var num = parseInt(target.text()) + 1
+    target.text(num)
+
+    // e.preventDefault();
+    // $.ajax({
+    //     url: "/:short_url",
+    //     method: "GET",
+    //     data: { 
+    //         click_count: $(this).val(), // < note use of 'this' here
+    //     },
+    //     dataType: 'json',
+
+    //     success: function(result) {
+    //       debugger
+    //         $('tr:first-child').after('<tr> <td>' + data.long_url + '</td> <td><a href="' + data.long_url + '">' + data.short_url + '</a></td> <td>' + data.click_count + '</td> </tr>')
+    //     },
+    //     error: function(result) {
+    //        $('#flash').html(data.responseText)  
+    //     }
+    });
+});
+
+
+ // end of function document.ready
+
+
 
